717b3f6dea601f6f5dfdfcfdaf19fdb9fdc14fe5
Allan Davis <javaalley@gmail.com>
2011-05-09 19:57:32 -0400

    initial project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DragonConnx::Application.config.secret_token = 'a885084dd9725813d0bcd5a4bcb76136aeb26f18deafc1a58d7533a77f13e31b705cfb1e96ee398c427453ea289769ed53dc36fc9bde3bca9f5316187d9bced7'
